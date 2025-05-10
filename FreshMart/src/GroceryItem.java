b1b9fb5da86ec866a934d4b1b9a7b76a52ed460b
“Karthick <karthickarun2009@gmail.com>
2025-05-10 15:37:32 -0700

    worked on runner class & certain subclasses

---
+++ FreshMart/src/GroceryItem.java
@@ -0,0 +1,20 @@
+public class GroceryItem {
+    String name;
+    double price;
+    int quantity;
+
+    public GroceryItem(String item, double cost, int qty){
+        this.name = item;
+        this.price = cost;
+        this.quantity = qty;
+    }
+    public String getName() { //accessor method for returning the item's name
+        return name;
+    }
+    public double getPrice() { //accessor method for returning the item's price
+        return price;
+    }
+    public int getQty() { //accessor method for returning how much there is in stock of the item
+        return quantity;
+    }
+}
