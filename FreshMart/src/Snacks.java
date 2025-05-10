a1998ec18fbaed278836e2eed36858f9ea0d01a3
“Karthick <karthickarun2009@gmail.com>
2025-05-10 16:33:56 -0700

    adding snacks constructor

--- FreshMart/src/Snacks.java
+++ FreshMart/src/Snacks.java
@@ -1,3 +1,5 @@
 public class Snacks extends GroceryItem {
-    
+    public Snacks(String snackname, double snackprice, int snackqty){
+        super(snackname,"Snack", snackprice, snackqty);
+    }
 }
